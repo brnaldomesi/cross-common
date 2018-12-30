f8eb29e1b9b8b35db67375eed397878ef86df8eb
brnaldomesi <brnaldomesi1@gmail.com>
2018-12-30 14:42:40 +0300

    string helper added

---
+++ Sources/Snappy.Common/Helpers/StringHelper.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Mail;
+using System.Text.RegularExpressions;
+
+namespace Snappy.Common.Helpers
+{
+    public static class StringHelper
+    {
+        public static bool IsAlphabeticalOrUnderscore(this string text)
+        {
+            var isAlphabetical = new Regex(@"[a-z_]+");
+
+            var isValidated = isAlphabetical.IsMatch(text);
+            return isValidated;
+        }
+
+        /// <summary>
+        /// Checks if a string contains number, upper case, lower case and more than 7 character 
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static bool IsValidPassword(this string text)
+        {
+            var hasNumber = new Regex(@"[0-9]+");
+            var hasLowerChar = new Regex(@"[a-z]+");
+            var hasUpperChar = new Regex(@"[A-Z]+");
+
+            var isValidated = hasNumber.IsMatch(text)
+                              && hasUpperChar.IsMatch(text)
+                              && hasLowerChar.IsMatch(text)
+                              && text.Length > 7;
+            return isValidated;
+        }
+
+        public static bool IsNotValidPassword(this string text)
+        {
+            return !IsValidPassword(text);
+        }
+
+        public static bool IsEmail(this string text)
+        {
+            try
+            {
+                var mailAddress = new MailAddress(text);
+                return mailAddress.Address == text;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static bool IsNotEmail(this string text)
+        {
+            return !IsEmail(text);
+        }
+
+        public static bool IsUid(this string text)
+        {
+            var isValid = Guid.TryParse(text, out Guid uid);
+            return isValid;
+        }
+
+        public static bool IsNotUid(this string text)
+        {
+            return !IsUid(text);
+        }
+
+        public static void ValidateUid(this string text)
+        {
+            if (IsUid(text))
+            {
+                return;
+            }
+
+            var method = new StackTrace().GetFrame(1).GetMethod();
+
+            throw new ArgumentException($"the uid is not valid > {text} [{method.DeclaringType}.{method.Name}]");
+        }
+
+        public static string GetNewUid()
+        {
+            var uid = Guid.NewGuid().ToString("N").ToUpper();
+            return uid;
+        }
+
+        public static bool IsEmpty(this string text)
+        {
+            return string.IsNullOrWhiteSpace(text);
+        }
+
+        public static bool IsNotEmpty(this string text)
+        {
+            return !IsEmpty(text);
+        }
+
+        public static bool IsUrl(this string text)
+        {
+            var result = Uri.TryCreate(text, UriKind.Absolute, out var uriResult)
+                         && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+
+            return result;
+        }
+
+        public static bool IsNotUrl(this string text)
+        {
+            return !IsUrl(text);
+        }
+    }
+}
