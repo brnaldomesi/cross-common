cd7b6988bdfee50b1207070ccfdd3b440895a503
brnaldomesi <brnaldomesi1@gmail.com>
2018-12-30 16:21:26 +0300

    DateTimeHelper New Tests Added

--- Tests/Snappy.Common.Tests/Helpers/DatetimeHelperTests.cs
+++ Tests/Snappy.Common.Tests/Helpers/DatetimeHelperTests.cs
@@ -1,13 +1,18 @@
 ﻿using NodaTime;
 using NodaTime.Testing;
+using NodaTime.Text;
 using NUnit.Framework;
 
 using Snappy.Common.Helpers;
 
 namespace Snappy.Common.Tests.Helpers
 {
+    [TestFixture]
     public class DatetimeHelperTests
     {
+        public const string DATE_FORMAT_PATTERN = "yyyy-MM-dd";
+        public const string TIME_FORMAT_PATTERN = "yyyy-MM-dd-HH-mm";
+
         [Test]
         public void DatetimeHelper_GetNow()
         {
@@ -21,6 +26,45 @@ namespace Snappy.Common.Tests.Helpers
             Assert.AreEqual(result, GetFakeNow());
         }
 
+        [Test]
+        public void DatetimeHelper_GetString()
+        {
+            // arrange
+            var helper = GetDatetimeHelper();
+
+            // act
+            var result = helper.GetString(helper.GetNow(),DATE_FORMAT_PATTERN);
+
+            // assert
+            Assert.AreEqual(result, InstantPattern.CreateWithInvariantCulture(DATE_FORMAT_PATTERN).Format(GetFakeNow()));
+        }
+
+        [Test]
+        public void DatetimeHelper_GetNowAsString()
+        {
+            // arrange
+            var helper = GetDatetimeHelper();
+
+            // act
+            var result = helper.GetNowAsString(DATE_FORMAT_PATTERN);
+
+            // assert
+            Assert.AreEqual(result, InstantPattern.CreateWithInvariantCulture(TIME_FORMAT_PATTERN).Format(GetFakeNow()));
+        }
+
+        [Test]
+        public void DatetimeHelper_GetTodayAsString()
+        {
+            // arrange
+            var helper = GetDatetimeHelper();
+
+            // act
+            var result = helper.GetTodayAsString(DATE_FORMAT_PATTERN);
+
+            // assert
+            Assert.AreEqual(result, InstantPattern.CreateWithInvariantCulture(DATE_FORMAT_PATTERN).Format(GetFakeNow()));
+        }
+
         private Instant GetFakeNow()
         {
             return Instant.FromUtc(2018, 12, 31, 10, 20, 30);
@@ -32,5 +76,6 @@ namespace Snappy.Common.Tests.Helpers
             var helper = new DatetimeHelper(clock);
             return helper;
         }
+
     }
 }
