78ffef1ed091131c6b7e514bd022af1d53993ff1
brnaldomesi <brnaldomesi1@gmail.com>
2019-01-09 16:43:00 +0300

    added paging info

---
+++ Sources/Snappy.Common/Models/Response/BaseResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace Snappy.Common.Models.Response
+{
+    public class BaseResponse
+    {
+        
+    }
+}
---
+++ Sources/Snappy.Common/Models/Shared/PagingInfo.cs
@@ -0,0 +1,20 @@
+﻿namespace Snappy.Common.Models.Shared
+{
+    public class PagingInfo
+    {
+        public int Skip { get; set; }
+        public int Take { get; set; }
+
+        public int PageItemCount { get; set; }
+        public long TotalItemCount { get; set; }
+
+        public int CurrentPage { get; set; }
+        public int PageCount { get; set; }
+
+        public long LastId { get; set; }
+        public long FirstId { get; set; }
+
+        public bool IsHavingPrevious { get; set; }
+        public bool IsHavingNext { get; set; }
+    }
+}
