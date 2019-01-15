4d0a4303e40241963c44e22f0eb5986b84438f74
brnaldomesi <brnaldomesi1@gmail.com>
2019-01-15 20:44:25 +0300

    importance enum added

---
+++ Sources/Snappy.Common/Enumerations/Importance.cs
@@ -0,0 +1,12 @@
+﻿namespace Snappy.Common.Enumerations
+{
+    public class Importance : Enumeration
+    {
+        public static readonly Importance Unknown = new Importance(0, "Unknown");
+        public static readonly Importance High = new Importance(1, "High");
+        public static readonly Importance Normal = new Importance(2, "Normal");
+        public static readonly Importance Low = new Importance(3, "Low");
+
+        private Importance(int value, string displayName) : base(value, displayName) { }
+    }
+}
---
+++ Tests/Snappy.Common.Tests/Enumerations/ImportanceTests.cs
@@ -0,0 +1,62 @@
+﻿using NUnit.Framework;
+
+using Snappy.Common.Enumerations;
+
+namespace Snappy.Common.Tests.Enumerations
+{
+    [TestFixture]
+    public class ImportanceTests
+    {
+        [Test]
+        public void Importance_Unknown()
+        {
+            var item = Importance.Unknown;
+
+            Assert.AreEqual(0, item.Value);
+            Assert.AreEqual("Unknown", item.DisplayName);
+            Assert.AreEqual(string.Empty, item.Description);
+        }
+
+        [Test]
+        public void Importance_High()
+        {
+            var item = Importance.High;
+
+            Assert.AreEqual(1, item.Value);
+            Assert.AreEqual("High", item.DisplayName);
+            Assert.AreEqual(string.Empty, item.Description);
+        }
+
+        [Test]
+        public void Importance_Normal()
+        {
+            var item = Importance.Normal;
+
+            Assert.AreEqual(2, item.Value);
+            Assert.AreEqual("Normal", item.DisplayName);
+            Assert.AreEqual(string.Empty, item.Description);
+        }
+
+        [Test]
+        public void Importance_Low()
+        {
+            var item = Importance.Low;
+
+            Assert.AreEqual(3, item.Value);
+            Assert.AreEqual("Low", item.DisplayName);
+            Assert.AreEqual(string.Empty, item.Description);
+        }
+
+        [Test]
+        public void Importance_Has_4_Items()
+        {
+            var items = Enumeration.GetAll<Importance>();
+
+            Assert.AreEqual(4, items.Count);
+            Assert.True(items.Contains(Importance.Unknown));
+            Assert.True(items.Contains(Importance.High));
+            Assert.True(items.Contains(Importance.Normal));
+            Assert.True(items.Contains(Importance.Low));
+        }
+    }
+}
