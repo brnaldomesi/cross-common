0e0ebfe27b5982b3facc7a53a9be43f9f7ac6b0c
brnaldomesi <brnaldomesi1@gmail.com>
2019-02-09 13:25:02 +0300

    response creation work

--- Sources/Snappy.Common/Models/Responses/BaseResponse.cs
+++ Sources/Snappy.Common/Models/Responses/BaseResponse.cs
@@ -1,7 +1,14 @@
-﻿namespace Snappy.Common.Models.Responses
+﻿using Snappy.Common.Enumerations;
+
+namespace Snappy.Common.Models.Responses
 {
     public abstract class BaseResponse
     {
-        
+        public ResponseStatus Status { get; set; }
+
+        protected BaseResponse()
+        {
+            Status = ResponseStatus.Unknown;
+        }
     }
 }
