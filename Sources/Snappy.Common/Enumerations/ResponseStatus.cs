65fe0629428c69e90bcca3a321c5a50ecb9cd756
brnaldomesi <brnaldomesi1@gmail.com>
2019-02-19 14:41:40 +0300

    label service review

--- Sources/Snappy.Common/Enumerations/ResponseStatus.cs
+++ Sources/Snappy.Common/Enumerations/ResponseStatus.cs
@@ -8,10 +8,19 @@ namespace Snappy.Common.Enumerations
         public static readonly ResponseStatus Success = new ResponseStatus(1, "Success");
         public static readonly ResponseStatus Failed = new ResponseStatus(2, "Failed");
         public static readonly ResponseStatus Invalid = new ResponseStatus(3, "Invalid");
+        public static readonly ResponseStatus SuccessWithProblems = new ResponseStatus(4, "SuccessWithProblems");
 
         private ResponseStatus(int value, string displayName) : base(value, displayName) { }
 
-        public bool IsSuccess { get { return Value == Success.Value && DisplayName == Success.DisplayName; } }
+        public bool IsSuccess
+        {
+            get
+            {
+                return Value == Success.Value 
+                       || Value == SuccessWithProblems.Value;
+            }
+        }
+
         public bool IsNotSuccess { get { return !IsSuccess; } }
     }
 }
