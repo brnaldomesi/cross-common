b10f470f1cedf1ac6ff1a4927a883d9c00aba6f9
brnaldomesi <brnaldomesi1@gmail.com>
2019-02-22 17:44:15 +0300

    its build

--- Sources/Snappy.Common/Models/DataTransferObjects/BaseDto.cs
+++ Sources/Snappy.Common/Models/DataTransferObjects/BaseDto.cs
@@ -7,11 +7,6 @@ namespace Snappy.Common.Models.DataTransferObjects
     public abstract class BaseDto
     {
         public Guid Uid { get; set; }
-        public string UidString
-        {
-            get { return Uid.ToString("N"); }
-        }
-
         public string Name { get; set; }
 
         public Instant CreatedAt { get; set; }
