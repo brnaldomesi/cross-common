fe4e5e36b844d61ed7ece44c07e842fd8450a27f
brnaldomesi <brnaldomesi1@gmail.com>
2019-02-22 14:03:28 +0300

    request review

--- Sources/Snappy.Common/Models/DataTransferObjects/BaseDto.cs
+++ Sources/Snappy.Common/Models/DataTransferObjects/BaseDto.cs
@@ -1,9 +1,19 @@
-﻿using NodaTime;
+﻿using System;
+
+using NodaTime;
 
 namespace Snappy.Common.Models.DataTransferObjects
 {
     public abstract class BaseDto
     {
+        public Guid Uid { get; set; }
+        public string UidString
+        {
+            get { return Uid.ToString("N"); }
+        }
+
+        public string Name { get; set; }
+
         public Instant CreatedAt { get; set; }
         public Instant? UpdatedAt { get; set; }
     }
---
+++ Sources/Snappy.Common/Models/Shared/ClientLogInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace Snappy.Common.Models.Shared
+{
+    public class ClientLogInfo
+    {
+        public string Platform { get; set; }
+        public string UserAgent { get; set; }
+        public string Browser { get; set; }
+        public string Ip { get; set; }
+        public string IpLocation { get; set; }
+    }
+}
