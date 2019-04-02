67b05084694fe37f8703dca0ef09c3ce26e508bd
brnaldomesi <brnaldomesi1@gmail.com>
2019-04-02 18:46:13 +0300

    paging

--- Sources/Snappy.Common/Models/Shared/PagingInfo.cs
+++ Sources/Snappy.Common/Models/Shared/PagingInfo.cs
@@ -2,19 +2,29 @@
 {
     public class PagingInfo
     {
+        public const string NEXT_PREVIOUS = "next_previous";
+        public const string PAGE_NUMBERS = "page_numbers";
+
         public int Skip { get; set; }
         public int Take { get; set; }
 
         public string LastUid { get; set; }
         public bool IsAscending { get; set; }
-
-        public int PageItemCount { get; set; }
+        
         public long TotalItemCount { get; set; }
 
         public int CurrentPage { get; set; }
-        public int PageCount { get; set; }
 
         public bool IsHavingPrevious { get; set; }
         public bool IsHavingNext { get; set; }
+        
+        public string Type { get; set; }
+
+        public PagingInfo()
+        {
+            LastUid = string.Empty;
+            IsAscending = true;
+            Take = 100;
+        }
     }
 }
