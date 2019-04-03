b5f8c7ef4546c776c2dce8d3918c3e4f2d1a717e
brnaldomesi <brnaldomesi1@gmail.com>
2019-04-03 10:41:30 +0300

    paging works

--- Sources/Snappy.Common/Models/Shared/PagingInfo.cs
+++ Sources/Snappy.Common/Models/Shared/PagingInfo.cs
@@ -4,21 +4,40 @@
     {
         public const string NEXT_PREVIOUS = "next_previous";
         public const string PAGE_NUMBERS = "page_numbers";
+        public string Type { get; set; }
 
         public int Skip { get; set; }
         public int Take { get; set; }
 
         public string LastUid { get; set; }
         public bool IsAscending { get; set; }
-        
+
         public long TotalItemCount { get; set; }
 
-        public int CurrentPage { get; set; }
+        public int CurrentPage {
+            get
+            {
+                if (Skip < 1)
+                {
+                    return 1;
+                }
 
-        public bool IsHavingPrevious { get; set; }
-        public bool IsHavingNext { get; set; }
-        
-        public string Type { get; set; }
+                return Skip / Take;
+            }
+        }
+
+        public bool IsHavingPrevious
+        {
+            get { return Skip > 0; }
+        }
+
+        public bool IsHavingNext
+        {
+            get
+            {
+                return TotalItemCount > Skip;
+            }
+        }
 
         public PagingInfo()
         {
