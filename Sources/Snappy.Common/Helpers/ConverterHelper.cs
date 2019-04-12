593feed68656be760378cf045b4ab0f45efddc61
brnaldomesi <brnaldomesi1@gmail.com>
2019-04-12 11:20:45 +0300

    converter helper implemented

---
+++ Sources/Snappy.Common/Helpers/ConverterHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Snappy.Common.Helpers
+{
+    public static class ConverterHelper
+    {
+        public static double ConvertBytesToMegabytes(this long bytes)
+        {
+            return Math.Round(((bytes / 1024f) / 1024f), 2);
+        }
+    }
+}
