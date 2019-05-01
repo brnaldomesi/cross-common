e268ec54a1dea4500a2e1f6ede5ce85dc8b9c7ef
brnaldomesi <brnaldomesi1@gmail.com>
2019-05-01 11:38:09 +0300

    registered journal service and attribute

---
+++ Sources/Snappy.Common/Exceptions/JournalException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Snappy.Common.Exceptions
+{
+    public class JournalException : Exception
+    {
+        public JournalException(string message) : base(message)
+        {
+
+        }
+    }
+}
--- Sources/Snappy.Common/Models/Shared/CurrentUser.cs
+++ Sources/Snappy.Common/Models/Shared/CurrentUser.cs
@@ -12,6 +12,8 @@ namespace Snappy.Common.Models.Shared
 
         public long Id { get; set; }
         public Guid Uid { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
         public string ObfuscationSalt { get; set; }
 
         public List<RoleInfo> Roles { get; set; }
