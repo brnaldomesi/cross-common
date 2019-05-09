855f900195a4782ca79a43fde8bb2b2647b329ca
brnaldomesi <brnaldomesi1@gmail.com>
2019-05-09 18:38:15 +0300

    model cleanup

--- Sources/Snappy.Common/Models/Shared/ClientLogInfo.cs
+++ Sources/Snappy.Common/Models/Shared/ClientLogInfo.cs
@@ -2,10 +2,13 @@
 {
     public class ClientLogInfo
     {
-        public string Platform { get; set; }
         public string UserAgent { get; set; }
+        public string Platform { get; set; }
+        public string PlatformVersion { get; set; }
         public string Browser { get; set; }
+        public string BrowserVersion { get; set; }
         public string Ip { get; set; }
-        public string IpLocation { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
     }
 }
