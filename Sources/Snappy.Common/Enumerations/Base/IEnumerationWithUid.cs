4ba428710218245702f6080eb2d07759111ed912
brnaldomesi <brnaldomesi1@gmail.com>
2019-05-30 14:31:28 +0300

    enumeration work

---
+++ Sources/Snappy.Common/Enumerations/Base/IEnumerationWithUid.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Snappy.Common.Enumerations.Base
+{
+    public interface IEnumerationWithUid
+    {
+        Guid Uid { get;  }
+        int Value { get; }
+        string DisplayName { get; }
+    }
+}
