76e1465cd63e5f0927172cd2a600e38332246797
brnaldomesi <brnaldomesi1@gmail.com>
2019-06-17 15:33:05 +0300

    upload folder csv works

---
+++ Sources/Snappy.Common/Models/Requests/FolderListInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace Snappy.Core.Client.Web.Models.Folder
+{
+    public class FolderListInfo
+    {
+        public string ParentFolderName { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
