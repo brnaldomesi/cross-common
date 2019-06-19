924af21d8736f836a84efeb175a1bcb490cbae40
brnaldomesi <brnaldomesi1@gmail.com>
2019-06-19 17:44:58 +0300

    refactoring (BaseDto)

--- Sources/Snappy.Common/Models/DataTransferObjects/BaseDto.cs
+++ Sources/Snappy.Common/Models/DataTransferObjects/BaseDto.cs
@@ -8,8 +8,10 @@ namespace Snappy.Common.Models.DataTransferObjects
         public string Name { get; set; }
 
         public Guid CreatedBy { get; set; }
+        public string CreatedByName { get; set; }
         public DateTime CreatedAt { get; set; }
         public Guid? UpdatedBy { get; set; }
+        public string UpdatedByName { get; set; }
         public DateTime? UpdatedAt { get; set; }
     }
 }
