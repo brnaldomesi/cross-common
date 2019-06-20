50da14197f2676aec1bd37264776107cdab9ddd2
brnaldomesi <brnaldomesi1@gmail.com>
2019-06-20 10:07:51 +0300

    revisiondto

---
+++ Sources/Snappy.Common/Models/DataTransferObjects/RevisionDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Snappy.Common.Models.DataTransferObjects
+{
+    public class RevisionDto<T> where T : BaseDto
+    {
+        public int Revision { get; set; }
+        public Guid RevisionedByUid { get; set; }
+        public string RevisionedByName { get; set; }
+        public DateTime RevisionedAt { get; set; }
+
+        public T Item { get; set; }
+    }
+}
