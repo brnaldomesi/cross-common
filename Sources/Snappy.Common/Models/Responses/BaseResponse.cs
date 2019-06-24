2e3176de390456e55e168f41f1e2d121b7852152
brnaldomesi <brnaldomesi1@gmail.com>
2019-06-24 16:18:00 +0300

    base response update

--- Sources/Snappy.Common/Models/Responses/BaseResponse.cs
+++ Sources/Snappy.Common/Models/Responses/BaseResponse.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 using Snappy.Common.Enumerations;
 using Snappy.Common.Models.DataTransferObjects;
@@ -57,6 +58,7 @@ namespace Snappy.Common.Models.Responses
 
     public abstract class BaseResponse<T> : BaseResponse where T : BaseDto, new()
     {
+        public Guid ItemUid { get; set; }
         public T Item { get; set; }
         public List<T> Items { get; set; }
 
