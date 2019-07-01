568abdbffe0f6fc58e00495189c3030414aaf558
brnaldomesi <brnaldomesi1@gmail.com>
2019-07-01 14:51:50 +0300

    token response updated

--- Sources/Snappy.Common/Models/Responses/TokenResponse.cs
+++ Sources/Snappy.Common/Models/Responses/TokenResponse.cs
@@ -5,6 +5,7 @@ namespace Snappy.Common.Models.Responses
     [Serializable]
     public class TokenResponse
     {
+        public bool isOk { get; set; }
         public Guid token { get; set; }
     }
 }
